e4345e577d16112095b02d3f61b9f47450a63db6
nanopilo <nanopilo@hotmail.com>
2016-09-27 13:56:04 -0500

    Modificación de la polinomial

--- src/montecarlosimulacion/C_Multiplicativo.java
+++ src/montecarlosimulacion/C_Multiplicativo.java
@@ -5,46 +5,117 @@
  */
 package montecarlosimulacion;
 
+import java.text.DecimalFormat;
+
 /**
  *
  * @author Hans Gomez
  */
 public class C_Multiplicativo {
-    //int a=5;
-    //int xn=4;
-    //int mod= 8;
-    double arreglo1[] = new double[20000];
-    boolean booleano[] = new boolean[20000];
-
+    //int a=3;
+    //int xn=17;
+    //int mod=100;
+    
     double Xi[] = new double[20000];
     double Yi [] = new double [20000];
+    boolean booleano[] = new boolean[20000];
+    DecimalFormat df = new DecimalFormat("0.0000"); 
+    private int coeficienteA,coeficienteB,coeficienteC ;
+    private int exponencialX;
+    private int valorX;
+    private String cadenaPolinomio;
+
+    public int getCoeficienteA() {
+        return coeficienteA;
+    }
+
+    public void setCoeficienteA(int coeficienteA) {
+        this.coeficienteA = coeficienteA;
+    }
+
+    public int getCoeficienteB() {
+        return coeficienteB;
+    }
+
+    public void setCoeficienteB(int coeficienteB) {
+        this.coeficienteB = coeficienteB;
+    }
+
+    public int getCoeficienteC() {
+        return coeficienteC;
+    }
+
+    public void setCoeficienteC(int coeficienteC) {
+        this.coeficienteC = coeficienteC;
+    }
+
+    public int getExponencialX() {
+        return exponencialX;
+    }
+
+    public void setExponencialX(int exponencialX) {
+        this.exponencialX = exponencialX;
+    }
+
+    public int getValorX() {
+        return valorX;
+    }
+
+    public void setValorX(int valorX) {
+        this.valorX = valorX;
+    }
     
     public void Multiplicativo(int a, int X0, int mod) {
+        cadenaPolinomio=""+coeficienteA+"("+valorX+")^"+exponencialX+"+"+coeficienteB+"("+valorX+")"+coeficienteC+"";
+        System.out.println(cadenaPolinomio);
+        double X1=(double)X0/100;
         if (X0 > 0 && a > 0 && mod > 0) {
-            double convertir;
+           
             if (a != X0) {
+              System.out.println("    "+"X2"+"     "+"Y");
                 for (int i = 1; i <= mod; i++) {
+                    
                     X0 = (X0 * a) % mod;
-                    convertir = (double) X0 / mod;
-                    Xi[i] = convertir;
+                    double convertir = (double) X0 / mod;
+                    Xi[i] = Math.pow(convertir, 2);
+                    
                     X0 = (X0 * a) % mod;
                     convertir = (double) X0 / mod;
                     Yi[i] = convertir;
                     
-                     if(Yi[i] < Math.pow(Xi[i], 2)){
+                    if(Yi[i]<Xi[i]){
                         booleano[i]=true;
-                    }
-                    else{
+                        //se agrega a los puntos debajo de la curva
+                    }else{
                         booleano[i]=false;
+                        //se agrega a los puntos encima de la curva
+
                     }
-                    System.out.println(Xi[i] + "  "+ Yi[i] + "  "+booleano[i]);
-                   
+                    if (X1 !=convertir){
+                           
+//                           if(i%2==0){
+//                               Xi[i]=convertir;
+//                           }
+//                           else{
+//                               Yi[i]=convertir;
+//                           }
+                        
+                        System.out.println("  "+df.format(Xi[i])+"   "+Yi[i]+ "  "+booleano[i]);
+                       
+                       }else{
+                           System.out.println("No tiene periodo maximo");
+                           break;
+                       }
                 }
-            } else {
-                System.out.println("La semilla es igual a A");
+               
+                }else {
+            System.out.println("IGUALDAD");
+        
             }
+        
         } else {
-            System.out.println("Los valores tienen que ser mayores que 0 Enteros");
-        }
-    }
+            System.out.println("LOS VALORES TIENEN QUE SER MAYOR A 0");
+}
+}
+    
 }
